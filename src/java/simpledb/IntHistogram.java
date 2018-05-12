f24b89d9c662e47bd9b12b87ac02f95413f565c2
kyowill <kyowill1990@gmail.com>
2018-05-12 08:44:16 +0800

    3.1

--- src/java/simpledb/IntHistogram.java
+++ src/java/simpledb/IntHistogram.java
@@ -1,9 +1,17 @@
 package simpledb;
 
+import java.util.HashMap;
+
 /** A class to represent a fixed-width histogram over a single integer-based field.
  */
 public class IntHistogram {
 
+	private int buckets;
+	private int min;
+	private int max;
+	private int interval;
+	private HashMap<Integer, HashMap<Integer, Integer>> histogram;
+	private int ntups;
     /**
      * Create a new IntHistogram.
      * 
@@ -22,6 +30,17 @@ public class IntHistogram {
      */
     public IntHistogram(int buckets, int min, int max) {
     	// some code goes here
+    	this.buckets = buckets;
+    	this.min = min;
+    	this.max = max;
+    	this.interval = (int) Math.ceil((double)(max - min + 1) / buckets);
+    	this.ntups = 0;
+    	histogram = new HashMap<Integer, HashMap<Integer, Integer>>();
+    	int key = 0;
+    	while(key < buckets){
+    		histogram.put(key, new HashMap<Integer, Integer>());
+    		key += 1;
+    	}
     }
 
     /**
@@ -30,6 +49,23 @@ public class IntHistogram {
      */
     public void addValue(int v) {
     	// some code goes here
+    	
+    	// int key = (v - min + 1) / interval;
+    	int idx = (int) Math.ceil((double)(v - min + 1) / interval) - 1;
+    	HashMap<Integer, Integer> map = histogram.get(idx);
+    	if(map == null){
+    		System.out.println("add value error!");
+    		return;
+    	}
+    	
+    	Integer val = map.get(idx);
+    	if(val != null){
+    		map.put(val, val.intValue() + 1);
+    		System.out.println("+1");
+    	}else{
+    		map.put(v, new Integer(1));
+    	}
+    	ntups += 1;
     }
 
     /**
@@ -45,7 +81,52 @@ public class IntHistogram {
     public double estimateSelectivity(Predicate.Op op, int v) {
 
     	// some code goes here
-        return -1.0;
+        //return -1.0;
+    	//int key = (v - min + 1) / interval;
+    	if(v < min){
+    		v = min;
+    	}
+    	if(v > max){
+    		v = max;
+    	}
+    	int key = (int) Math.ceil((double)(v - min + 1) / interval) - 1;
+    	HashMap<Integer, Integer> map = histogram.get(new Integer(key));
+    	if(map == null){
+    		System.out.println("key not exist error!");
+    		System.out.println(key + ":" + v + ":" + max + ":" + min + ":" + buckets);
+    		return -1.0;
+    	}
+    	if(op.toString().equals("=")){
+        	double h = (double) map.size();
+        	double w = (double) interval;
+        	return (h / w) / ntups;
+    	}else if(op.toString().equals(">")){
+    		int right = min + (key + 1) * interval;
+    		int part = (right - v) / interval;
+    		int h = map.size();
+    		int w = interval;
+    		int big = key + 1;
+    		double num = (double) h * part / interval;
+    		while((big) < histogram.size()){
+    			num += histogram.get(big).size();
+    			big += 1;
+    		}
+    		return num / ntups;
+    	}else if (op.toString().equals("<")){
+    		int left = min + key * interval;
+    		int part = (v - left) / interval;
+    		int h = map.size();
+    		int w = interval;
+    		int small = key - 1;
+    		double num = (double) h * part / interval;
+    		while((small) >= 0){
+    			num += histogram.get(small).size();
+    			small -= 1;
+    		}
+    		return num / ntups;
+    	}else {
+    		return -1.0;
+    	}
     }
     
     /**
@@ -67,6 +148,7 @@ public class IntHistogram {
      */
     public String toString() {
         // some code goes here
-        return null;
+        //return null;
+    	return buckets + ":" + min + ":" + max + ":" + interval;
     }
 }
