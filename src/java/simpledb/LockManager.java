33768c07e050154a1e7700bcccca822aa555501c
kyowill <291453783@qq.com>
2018-07-13 18:46:10 +0800

    4.1

--- src/java/simpledb/LockManager.java
+++ src/java/simpledb/LockManager.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -14,55 +15,56 @@ public class LockManager {
 	private final ConcurrentHashMap<PageId, Object> locks;
 	private final Map<PageId, Set<TransactionId>> readLockHolders;
 	private final Map<PageId, TransactionId> writeLockHolders;
-	private final Map<TransactionId, Set<PageId>> dirtyPages; 
+	private final Map<TransactionId, List<PageId>> dirtyPages; 
 	
 	public LockManager(){
 		locks = new ConcurrentHashMap<PageId, Object>();
 		readLockHolders = new ConcurrentHashMap<PageId, Set<TransactionId>>();
 		writeLockHolders = new ConcurrentHashMap<PageId, TransactionId>();
-		dirtyPages = new ConcurrentHashMap<TransactionId, Set<PageId>>();
+		dirtyPages = new ConcurrentHashMap<TransactionId, List<PageId>>();
 	}
 
 	public void acquireLock(TransactionId tid,Permissions perm, PageId pid) throws InterruptedException {
 		if(dirtyPages.get(tid) == null){
-			dirtyPages.put(tid, new HashSet<PageId>());
+			dirtyPages.put(tid, new LinkedList<PageId>());
 		}
 		if(readLockHolders.get(pid) == null){
 			readLockHolders.put(pid, new HashSet<TransactionId>());
+		}
+		if(isHoldLock(tid, pid)){
+			return;
 		}
 		if(perm.permLevel == 0){
 			acquireReadLock(tid, pid);
 		}else{
 			acquireWriteLock(tid, pid);
 		}
+		dirtyPages.get(tid).add(pid);
 	}
 
 	
 	public boolean releaseLock(TransactionId tid, PageId pid) throws InterruptedException{
-		return releaseWriteLock(tid, pid) || releaseReadLock(tid, pid);
+		if(!isHoldLock(tid, pid)){
+			return false;
+		}
+		boolean done = releaseWriteLock(tid, pid) || releaseReadLock(tid, pid);
+		dirtyPages.get(tid).remove(pid);
+		return done;
 	}
 	
 	private void acquireReadLock(TransactionId tid, PageId pid) throws InterruptedException {
-		if(isHoldLock(tid, pid)){
-			return;
-		}
 		locks.putIfAbsent(pid, tid);
 		synchronized (locks.get(pid)) {
 			while(writeLockHolders.get(pid) != null){
 				locks.get(pid).wait();
 			}
 			readLockHolders.get(pid).add(tid);
-			dirtyPages.get(tid).add(pid);
 		}
 	}
 	
 	private boolean releaseReadLock(TransactionId tid, PageId pid) throws InterruptedException{
-		if(!isHoldLock(tid, pid)){
-			return false;
-		}
 		synchronized (locks.get(pid)) {
 			readLockHolders.get(pid).remove(tid);
-			dirtyPages.get(tid).remove(pid);
 			if(readLockHolders.get(pid).size() == 0){
 				locks.get(pid).notifyAll();
 			}
@@ -71,26 +73,18 @@ public class LockManager {
 	}
 	
 	private void acquireWriteLock(TransactionId tid, PageId pid) throws InterruptedException{
-		if(isHoldLock(tid, pid)){
-			return;
-		}
 		locks.putIfAbsent(pid, tid);
 		synchronized (locks.get(pid)) {
 			while(readLockHolders.get(pid).size() != 0){
 				locks.get(pid).wait();
 			}
 			writeLockHolders.put(pid, tid);
-			dirtyPages.get(tid).add(pid);
 		}
 	}
 	
 	private boolean releaseWriteLock(TransactionId tid, PageId pid){
-		if(!isHoldLock(tid, pid)){
-			return false;
-		}
 		synchronized (locks.get(pid)) {
 			writeLockHolders.remove(pid, tid);
-			dirtyPages.get(tid).remove(pid);
 			locks.get(pid).notifyAll();
 			return true;
 		}
@@ -124,21 +118,16 @@ public class LockManager {
 	}
 	
 	public void releaseLock(TransactionId tid){
-		if(dirtyPages.get(tid) == null || dirtyPages.get(tid).size() == 0){
-			return;
-		}
-		Iterator<PageId> iter = dirtyPages.get(tid).iterator();
-		while(iter.hasNext()){
-			PageId pid = iter.next(); 
-			if(pid == null){
-				continue;
-			}
+		List<PageId> pageIds = dirtyPages.get(tid);
+		for(PageId pid: pageIds){
 			try {
+				System.out.println(pid);
 				releaseLock(tid, pid);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
+		dirtyPages.remove(tid);
 	}
 }
